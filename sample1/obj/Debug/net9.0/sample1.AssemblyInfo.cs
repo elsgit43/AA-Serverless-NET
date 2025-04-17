c22be18ac22fd46901e285571a0de9b6c4e5965c
Elias Salvan <51190948+elsgit43@users.noreply.github.com>
2025-04-17 09:04:44 +0000

    sample1.AssemblyInfo

--- sample1/obj/Debug/net9.0/sample1.AssemblyInfo.cs
+++ sample1/obj/Debug/net9.0/sample1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sample1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2992d74487852b6db1d6511b79590c82de6ec47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8e296aac08d57564c4f6d99a859a77520c9bf05")]
 [assembly: System.Reflection.AssemblyProductAttribute("sample1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sample1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
